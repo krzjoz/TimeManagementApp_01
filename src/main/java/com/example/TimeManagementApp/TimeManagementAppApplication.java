4f1a61cdd0394a1e6de90ea87831bf853b85e029
krzjoz <krz_joz@windowslive.com>
2020-08-10 15:27:30 +0200

    Add dependencies

---
+++ src/main/java/com/example/TimeManagementApp/TimeManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.TimeManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TimeManagementApp/TimeManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TimeManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
